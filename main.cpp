c4a75ccf15d297b31b653a4ba6e75973ca4dabfa
Demian Foliush <Demian.Foliush.IR.2021@lpnu.ua>
2022-09-02 23:51:52 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#import "RedBlackTree.cpp"
+
+int main() {
+    RBTree tree1;
+    tree1.insert(100);
+    tree1.insert(150);
+    tree1.insert(125);
+    tree1.insert(130);
+    tree1.insert(120);
+    tree1.insert(175);
+    tree1.insert(180);
+    return 0;
+}
